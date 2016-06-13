ae821109c2361d9ddb08f0bb6361eaa1c9818a1a
Haggai Philip Zagury <hagzag@tikalk.com>
2016-06-13 08:59:39 +0000

    Reafctor cleanups

--- test-env-provision.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -vv -i localhost, -e "type=artifactory" test-env-provision.yml
-ansible-playbook -vv -i localhost, -e "type=jenkins" test-env-provision.yml
-ansible-playbook -vv -i localhost, -e "type=gerrit" test-env-provision.yml
